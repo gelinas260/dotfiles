43b4b4a100bcf5a5d8ccedfdb74f287d98a6118a
Zachary Mattor <zmattor@gmail.com>
2014-01-28 21:37:35 -0500

    working on install script

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
-cp -r .vim ~/.vim
-cp -r .vimrc ~/.vimrc
+clear;
+echo -e "--Make sure you are running this from the repo path--\n\n";
+read -p "This will override your config files [Y/n]" -n 1 -r
+echo    # move to a new line
 
-echo "YOU NEED TO SET YOUR IRSSI ACCOUNT PASSWORD";
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	rm -rf ~/.vim
+	rm -rf ~/.irssi
+	rm -rf ~/.vimrc
+
+	ln -s ${PWD}/.vim ~/.vim
+	ln -s ${PWD}/.vimrc ~/.vimrc
+	cp -r ${PWD}/.irssi ~/.irssi
+
+	echo "YOU NEED TO SET YOUR IRSSI ACCOUNT PASSWORD (count not be directly linked due to personal information)";
+fi
