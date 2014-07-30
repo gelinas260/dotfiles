33a98a903490a2309398caf85ced29beb64e71d5
Zack Mattor <zmattor@gmail.com>
2014-07-29 22:23:54 -0400

    Fixing install script

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ then
   ln -s ${PWD}/.vim ~/.vim
   ln -s ${PWD}/.vimrc ~/.vimrc
   ln -s ${PWD}/.bash_profile ~/.bash_profile
-  ln -s ${PWD}/.tmux.config ~/.tmux.conf
+  ln -s ${PWD}/.tmux.conf ~/.tmux.conf
   cp -r ${PWD}/.irssi ~/.irssi
 
   echo "YOU NEED TO SET YOUR IRSSI ACCOUNT PASSWORD (count not be directly linked due to personal information)\n";
